3f8018f00b3892db5dc2510737c5a41e22345bf7
jan jakubowski <jan.jakubowski@gmail.com>
2019-07-21 20:25:18 +0200

    Draw2d integration with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+
+declare let draw2d:any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'draw2d-angular';
+
+  ngOnInit() {
+    var canvas = new draw2d.Canvas("gfx_holder");
+    // Create two standard nodes for "start" and "end" and link
+    // this figures with a standard Connector
+    //
+    var start = new draw2d.shape.node.Start();
+    var end = new draw2d.shape.node.End();
+
+    canvas.add(start, 80, 180);
+    canvas.add(end, 450, 250);
+
+    // Add a connection via API calls between Start and Stop
+    //
+    var connection = new draw2d.Connection();
+    connection.setSource(start.getOutputPort(0));
+    connection.setTarget(end.getInputPort(0));
+    canvas.add(connection);
+  }
+
 }
